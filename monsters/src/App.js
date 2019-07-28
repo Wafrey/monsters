6d6db4e4b681bf2dcd1feb663d97671e90e782a1
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-07-28 13:33:07 +0300

    Add card-list component

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state={
+     monsters: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=> res.json())
+    .then(users=> this.setState({monsters: users}));
+  }
+
+  changeText = () =>{
+    this.setState({string: 'Hello goshko'})
+  }
+
+  render(){
+    return(
+      
+      <div className='App'>
+      <CardList monsters={this.state.monsters} />   
+      </div>
+    )
+  }
 }
 
 export default App;
