d840a9142eecbd9b0b5280b516e652097ae6536b
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-07-28 15:57:44 +0300

    Add search input to component

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
 
 class App extends Component{
@@ -29,10 +30,9 @@ class App extends Component{
     )
     return(
       <div className='App'>
-        <input 
-        type='search' 
-        placeholder='search for monsters' 
-        onChange={e => this.setState({searchField: e.target.value})}         
+        <SearchBox
+          placeholder='search for monsters'
+          handleChange={e => this.setState({searchField: e.target.value})}
         />
         <CardList 
         monsters={filteredMonsters}          
