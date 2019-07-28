f4cdef34e59a4043a903368615f0844f37e764fb
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-07-28 19:59:55 +0300

    Add all the css and styles

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -30,6 +30,7 @@ class App extends Component{
     )
     return(
       <div className='App'>
+        <h1>Monsters</h1>
         <SearchBox
           placeholder='search for monsters'
           handleChange={e => this.setState({searchField: e.target.value})}
